cea9edfcfbbc66c7e7e2a346bd912f8442ac5f91
Ailene Tan <aileneymt@gmail.com>
2026-04-08 16:33:21 -0400

    Removed coupling between BlockAllocator and KVCache. engine will need to pass in block information into KVCache

--- kv_cache.py
+++ kv_cache.py
@@ -5,9 +5,8 @@ class KVCache:
     # holds the KV tensors
     # uses BlockAllocator to know where to store them
     # write and read from the KV cache
-    def __init__(self, allocator: BlockAllocator, total_blocks: int, block_size: int, num_layers: int, num_heads: int, head_dim: int):
+    def __init__(self, total_blocks: int, block_size: int, num_layers: int, num_heads: int, head_dim: int):
         shape = (total_blocks, block_size, num_heads, head_dim)
-        self.allocator = allocator
         self.k_cache = [torch.zeros(shape, device='cuda') for _ in range(num_layers)]
         self.v_cache = [torch.zeros(shape, device='cuda') for _ in range(num_layers)]
     '''
@@ -15,26 +14,21 @@ class KVCache:
     start_pos captures the offset in the entire k_cache, not just starting from a single block
     token_pos is the token's index in its own request
     '''
-    def write(self, request_id: int, layer_idx: int, token_pos: int, key, val):
+    def write(self, layer_idx: int, block_ids: list[int], token_pos: int, key, val):
         # key and val shape [12 (num_heads), 64 (head_dim)] since its for ONE token
-        if not self.allocator.request_exists(request_id):
-            raise LookupError(f"Request {request_id} does not exist")
-        
+
         block_size = len(self.k_cache[0][0])
         logical_block_idx = token_pos // block_size
-        phys_block_idx = self.allocator.block_table[request_id][logical_block_idx]
+        phys_block_idx = block_ids[logical_block_idx]
 
         offset = token_pos % block_size
         self.k_cache[layer_idx][phys_block_idx][offset] = key
         self.v_cache[layer_idx][phys_block_idx][offset] = val
     
-    def read(self, request_id: int, layer_idx: int, token_pos: int):
-        if not self.allocator.request_exists(request_id):
-            raise LookupError(f"Request {request_id} does not exist")
-        
+    def read(self, layer_idx: int, block_ids: list[int], token_pos: int):
         block_size = len(self.k_cache[0][0])
         logical_block_idx = token_pos // block_size
-        phys_block_idx = self.allocator.block_table[request_id][logical_block_idx]
+        phys_block_idx = block_ids[logical_block_idx]
         offset = token_pos % block_size
 
         return (self.k_cache[layer_idx][phys_block_idx][offset], self.v_cache[layer_idx][phys_block_idx][offset])
@@ -43,17 +37,13 @@ class KVCache:
     Will be used for writing prefill. instead of writing one token's key val tensors at a time, this will try to fill one block at a time
     delegates to write() if necessary
     '''
-    def write_batch(self, request_id: int, layer_idx: int, key, val):
-        if not self.allocator.request_exists(request_id):
-            raise LookupError(f"Request {request_id} does not exist")
-        
+    def write_batch(self, layer_idx: int, block_ids: list[int], key, val):
         block_size = len(self.k_cache[0][0])
-        block_list = self.allocator.block_table[request_id]
         token_pos = 0
         num_tokens = len(key)
         while token_pos < num_tokens:
             logical_block_idx = token_pos // block_size
-            phys_block_idx = block_list[logical_block_idx]
+            phys_block_idx = block_ids[logical_block_idx]
             tokens_in_block = min(num_tokens - token_pos, block_size)
             start, end = logical_block_idx*block_size, logical_block_idx*block_size + tokens_in_block
             
@@ -61,20 +51,15 @@ class KVCache:
             self.v_cache[layer_idx][phys_block_idx][:tokens_in_block] = val[start:end]
             token_pos += tokens_in_block
 
-    def read_batch(self, request_id: int, layer_idx: int):
-        if not self.allocator.request_exists(request_id):
-            raise LookupError(f"Request {request_id} does not exist")
-        
+    def read_batch(self, layer_idx: int, block_ids: list[int], num_tokens: int):
         k_list = []
         v_list = []
         block_size = len(self.k_cache[0][0])
-        block_list = self.allocator.block_table[request_id]
 
-        for log_block_idx, phys_block_id in enumerate(block_list):
-            tokens_in_block = self.allocator.all_blocks[phys_block_id].num_filled
-            end = log_block_idx * block_size + tokens_in_block
-            k_list.append(self.k_cache[layer_idx][phys_block_id][:end])
-            v_list.append(self.v_cache[layer_idx][phys_block_id][:end])
+        for log_block_idx, phys_block_id in enumerate(block_ids):
+            tokens_in_block = block_size if log_block_idx < len(block_ids) - 1 else num_tokens % block_size or block_size
+            k_list.append(self.k_cache[layer_idx][phys_block_id][:tokens_in_block])
+            v_list.append(self.v_cache[layer_idx][phys_block_id][:tokens_in_block])
         k_full = torch.cat(k_list, dim=0)
         v_full = torch.cat(v_list, dim=0)
         return (k_full, v_full)
